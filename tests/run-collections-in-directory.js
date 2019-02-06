ba4d3c19bab066b355cea2315fa3fb0b59ab93c5
Thirumurugan19 <thiru.m@gmail.com>
2019-02-06 10:51:01 +0530

    Update run-collections-in-directory.js

--- tests/run-collections-in-directory.js
+++ tests/run-collections-in-directory.js
@@ -2,27 +2,30 @@ var fs = require('fs'),
     async = require('async'), // https://npmjs.org/package/async
     newman = require('newman');
 
-fs.readdir('./', function (err, files) {
-    if (err) { throw err; }
+fs.readdir('./tests', function (err, files) {
+    if (err) { console.log("erroe", err); }
 
     // we filter all files with JSON file extension
+    
     files = files.filter(function (file) {
         return (file.substr(-5) === '.json');
     });
+    console.log("files....", files)
 
-    // now wer iterate on each file name and call newman.run using each file name
+    // now we iterate on each file name and call newman.run using each file name
     async.eachSeries(files, function (file, next) {
         newman.run({
             collection: require(`${__dirname}/${file}`),
 	    reporters: 'cli',
-	    environment: 'env.json',
-	    iterationData: 'testdata.csv'
+	    environment: './tests/env.json',
+	    iterationData: './tests/testdata.csv'
         }, function (err, summary) {
             // finally, when the collection executes, print the status
-            console.info(`${file}: ${err ? err.name : 'ok'}!`);
+            console.info(`${file}: ${err ? err : 'ok'}!`);
             next(err, summary);
         });
     }, function (err, results) {
        // process the errors/results here
+       console.log("error", err)
     });
 });
